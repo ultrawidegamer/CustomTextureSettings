3c748fb01e18fd29d73056b42741c87cbee25525
ultrawidegamer <147885362+ultrawidegamer@users.noreply.github.com>
2025-06-26 05:03:41 -0300

    Initial commit

---
+++ ExampleMod/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ExampleMod")]
+[assembly: AssemblyProduct("ExampleMod")]
+[assembly: AssemblyDescription("Description of your mod")]
+[assembly: AssemblyCompany("ExampleAuthor")]
+[assembly: AssemblyCopyright("Copyright © 2025 ExampleAuthor")]
+[assembly: AssemblyVersion(ExampleMod.ExampleMod.VERSION_CONSTANT)]
+[assembly: AssemblyFileVersion(ExampleMod.ExampleMod.VERSION_CONSTANT)]
