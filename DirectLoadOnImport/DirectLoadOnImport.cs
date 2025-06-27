421963149aea02c68f6ed46355a4cb819f07c6cb
ultrawidegamer <147885362+ultrawidegamer@users.noreply.github.com>
2025-06-27 16:15:32 -0300

    Image importer now adds a button that when clicked will import your image with direct load enabled

--- DirectLoadOnImport/DirectLoadOnImport.cs
+++ DirectLoadOnImport/DirectLoadOnImport.cs
@@ -4,6 +4,8 @@ using FrooxEngine;
 using FrooxEngine.UIX;
 using HarmonyLib;
 using ResoniteModLoader;
+using System.Collections.Generic;
+using System.Linq;
 
 #if DEBUG
 	using ResoniteHotReloadLib;
@@ -12,13 +14,15 @@ using ResoniteModLoader;
 namespace DirectLoadOnImport;
 
 public class DirectLoadOnImportMod : ResoniteMod {
-	internal const string VERSION_CONSTANT = "1.0.0"; //Changing the version here updates it in all locations needed
+	internal const string VERSION_CONSTANT = "1.0.0";
 	public override string Name => "DirectLoadOnImport";
 	public override string Author => "ultrawidegamer";
 	public override string Version => VERSION_CONSTANT;
 	public override string Link => "https://github.com/ultrawidegamer/DirectLoadOnImport";
 
 	const string HarmonyId = "com.ultrawidegamer.DirectLoadOnImport";
+
+	internal static readonly List<ImportItem?> ItemPaths = new List<ImportItem?>();
 
 	[AutoRegisterConfigKey]
 	private static readonly ModConfigurationKey<bool> enabled = new ModConfigurationKey<bool>("enabled", "Should the mod be enabled", () => true);
@@ -54,7 +58,24 @@ public class DirectLoadOnImportMod : ResoniteMod {
 		}
 	#endif
 
+	[HarmonyPatch(typeof(ImageImporter), "SetupTextureProxyComponents")]
+	class ImageImporter_SetupTextureProxyComponents_Patch {
+		[HarmonyPrefix]
+		private static bool SetupTextureProxyComponents(Slot targetSlot, IAssetProvider<Texture2D> texture, StereoLayout stereoLayout, ImageProjection projection, bool setupPhotoMetadata) {
+			if (Config is null) return true;
+			if (!Config.GetValue(enabled)) return true;
 
+			var slotName = targetSlot.NameField ?? "";
+			var match = ItemPaths.FirstOrDefault(path => path?.itemName == slotName);
+
+			if (match is null) return true;
+
+			targetSlot.GetComponent<StaticTexture2D>().DirectLoad.Value = true;
+			ItemPaths.Remove(match);
+
+			return true;
+		}
+	}
 
 	[HarmonyPatch(typeof(ImageImportDialog), "OpenRoot")]
 	class ImageImportDialog_OpenRoot_Patch {
@@ -63,14 +84,17 @@ public class DirectLoadOnImportMod : ResoniteMod {
 			if (Config is null) return;
 			if (!Config.GetValue(enabled)) return;
 
-			Msg("Postfix of ImageImportDialog");
-
 			LocaleString text = "DirectLoad";
 			Button directLoadButton = ui.Button(in text);
 
 			directLoadButton.LocalPressed += (IButton button, ButtonEventData eventData) => {
 				try {
-					Msg("Trying to import image");
+					foreach (var path in __instance.Paths) {
+						ItemPaths.Add(path);
+						Traverse.Create(__instance)
+							.Method("Preset_Image", new[] { typeof(IButton), typeof(ButtonEventData) })
+							.GetValue(button, eventData);
+					}
 				} catch (Exception error) {
 					directLoadButton.LabelText = $"<alpha=red>Failed to import. Check logs.";
 					Error(error);
