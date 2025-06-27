51577ba154cbff1c484f465a070f8d86190ad95c
ultrawidegamer <147885362+ultrawidegamer@users.noreply.github.com>
2025-06-26 21:04:28 -0300

    Added initial button to image import dialog and added support for hot reload lib

--- DirectLoadOnImport/DirectLoadOnImport.cs
+++ DirectLoadOnImport/DirectLoadOnImport.cs
@@ -1,6 +1,13 @@
+using System;
+using Elements.Core;
 using FrooxEngine;
+using FrooxEngine.UIX;
 using HarmonyLib;
 using ResoniteModLoader;
+
+#if DEBUG
+	using ResoniteHotReloadLib;
+#endif
 
 namespace DirectLoadOnImport;
 
@@ -11,28 +18,64 @@ public class DirectLoadOnImportMod : ResoniteMod {
 	public override string Version => VERSION_CONSTANT;
 	public override string Link => "https://github.com/ultrawidegamer/DirectLoadOnImport";
 
+	const string HarmonyId = "com.ultrawidegamer.DirectLoadOnImport";
+
 	[AutoRegisterConfigKey]
 	private static readonly ModConfigurationKey<bool> enabled = new ModConfigurationKey<bool>("enabled", "Should the mod be enabled", () => true);
 
-	private static ModConfiguration Config;
+	private static ModConfiguration? Config;
 
 	public override void OnEngineInit() {
-		Config = GetConfiguration();
-		Config.Save(true);
-		Harmony harmony = new Harmony("com.ultrawidegamer.DirectLoadOnImport");
-		harmony.PatchAll();
+		Init(this);		
 
-		Debug("a debug log");
-		Msg("a regular log");
-		Warn("a warn log");
-		Error("an error log");
+		#if DEBUG
+			HotReloader.RegisterForHotReload(this);
+		#endif
 	}
 
-	////Example of how a HarmonyPatch can be formatted, Note that the following isn't a real patch and will not compile.
-	//[HarmonyPatch(typeof(ClassNameHere), "MethodNameHere")]
-	//class ClassNameHere_MethodNameHere_Patch {
-	//	static void Postfix(ClassNameHere __instance) {
-	//		Msg("Postfix from ExampleMod");
-	//	}
-	//}
+	private static void Init(ResoniteMod instance) {
+		Config = instance?.GetConfiguration();
+
+		if (Config is null) return;
+
+		Config.Save(true);
+		Harmony harmony = new Harmony(HarmonyId);
+		harmony.PatchAll();
+	}
+
+	#if DEBUG
+		public static void BeforeHotReload() {
+			Harmony harmony = new Harmony(HarmonyId);
+			harmony.UnpatchAll(HarmonyId);
+		}
+
+		public static void OnHotReload(ResoniteMod instance) {
+			Init(instance);
+		}
+	#endif
+
+
+
+	[HarmonyPatch(typeof(ImageImportDialog), "OpenRoot")]
+	class ImageImportDialog_OpenRoot_Patch {
+		[HarmonyPostfix]
+		private static void OpenRoot(ImageImportDialog __instance, UIBuilder ui) {
+			if (Config is null) return;
+			if (!Config.GetValue(enabled)) return;
+
+			Msg("Postfix of ImageImportDialog");
+
+			LocaleString text = "DirectLoad";
+			Button directLoadButton = ui.Button(in text);
+
+			directLoadButton.LocalPressed += (IButton button, ButtonEventData eventData) => {
+				try {
+					Msg("Trying to import image");
+				} catch (Exception error) {
+					directLoadButton.LabelText = $"<alpha=red>Failed to import. Check logs.";
+					Error(error);
+				}
+			};
+		}
+	}
 }
